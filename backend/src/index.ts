07c65c5ecf733145c1bf06c9b87a3024a88de6aa
spyeatt <sampyeatt@gmail.com>
2025-08-29 13:58:05 -0400

    add user and auth functions

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express'
 import './database/index'
 import timeRoute from './routes/time.route'
-import {Request, Response} from 'express'
+import authRoutes from './routes/auth.routes'
+import userRoutes from './routes/user.routes'
 
 const app = express()
 const port: number = 3001
@@ -11,6 +12,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/time', timeRoute)
+app.use('/api/auth', authRoutes)
+app.use('/api/user', userRoutes)
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
