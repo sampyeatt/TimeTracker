eb9095271f55fc4c5af2bbae34338ed3a3f044c6
spyeatt <sampyeatt@gmail.com>
2025-09-20 14:50:50 -0400

    update theme

--- frontend/src/app/component/theme/theme.ts
+++ frontend/src/app/component/theme/theme.ts
@@ -7,21 +7,21 @@ export const DarkMode = definePreset(Aura, {
     colorScheme: {
       dark: {
         formField: {
-          hoverBorderColor: '{red.800}'
+          hoverBorderColor: '{purple.50}'
 
         },
         primary: {
-          50: '{red.50}',
-          100: '{red.100}',
-          200: '{red.200}',
-          300: '{red.300}',
-          400: '{red.400}',
-          500: '{red.500}',
-          600: '{red.600}',
-          700: '{red.700}',
-          800: '{red.800}',
-          900: '{red.900}',
-          950: '{red.950}'
+          50: '{purple.50}',
+          100: '{purple.100}',
+          200: '{purple.200}',
+          300: '{purple.300}',
+          400: '{purple.400}',
+          500: '{purple.500}',
+          600: '{purple.600}',
+          700: '{purple.700}',
+          800: '{purple.800}',
+          900: '{purple.900}',
+          950: '{purple.950}'
         }
       }
     }
@@ -31,7 +31,7 @@ export const DarkMode = definePreset(Aura, {
     button: {
       extend: {
         accent: {
-          color: '{red-600}'
+          color: '{purple-600}'
         }
       }, root: {
         raisedShadow: '0 6px 2px -4px rgba(0, 0, 0, 0.2), 0 4px 4px 0 rgba(0, 0, 0, 0.14), 0 2px 10px 0 rgba(0, 0, 0, 0.12)',
