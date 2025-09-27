6578fef0a00dfb5b3007334f46bc079e662d75da
spyeatt <sampyeatt@gmail.com>
2025-09-27 11:55:19 -0400

    update auth and db logic

---
+++ frontend/main-electron.ts
@@ -0,0 +1,33 @@
+import { app, BrowserWindow, screen } from 'electron'
+
+function createWindow () {
+  const size = screen.getPrimaryDisplay().workAreaSize
+  const win = new BrowserWindow({
+    x: 0,
+    y: 0,
+    width: size.width,
+    height: size.height,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true,
+    }
+  })
+
+  win.loadURL('http://localhost:4200/')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
