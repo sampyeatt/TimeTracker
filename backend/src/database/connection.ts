4a823c57ee1dcad7bca67fe1467e4a5df617f357
spyeatt <sampyeatt@gmail.com>
2025-11-26 18:25:22 -0500

    remove old files

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -2,6 +2,6 @@ import {Sequelize} from 'sequelize-typescript'
 
 export const connection = new Sequelize({
     dialect: 'sqlite',
-    storage: '../db/time.db',
+    storage: './time.db',
     models: [__dirname + '/../models']
 })
