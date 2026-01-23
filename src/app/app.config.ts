7723f4af4e74f7e6c65ef6bc3447d17f8c9eec7d
spyeatt <sampyeatt@gmail.com>
2026-01-23 10:51:56 -0500

    Update dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,23 +6,22 @@ import {
 import {provideRouter} from '@angular/router'
 import {DarkMode} from './component/theme/theme'
 import {providePrimeNG} from 'primeng/config'
-import {provideHttpClient, withFetch} from '@angular/common/http'
 import {provideClientHydration, withEventReplay} from '@angular/platform-browser'
 import {routes} from './app.routes'
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async'
-
+import Aura from '@primeuix/themes/aura'
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideBrowserGlobalErrorListeners(),
         provideZonelessChangeDetection(),
         provideRouter(routes),
-        provideHttpClient(withFetch()),
         provideAnimationsAsync(),
         provideClientHydration(withEventReplay()),
         providePrimeNG({
+            ripple: true,
             theme: {
-                preset: DarkMode
+                preset: Aura
             }
         })
     ]
